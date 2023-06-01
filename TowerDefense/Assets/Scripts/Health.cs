a99b00c7d23b457cc746c49ba5e2bf2a74c19f64
Newton Galiza <newtonjgaliza@gmail.com>
2023-06-01 17:30:25 -0300

    part 6
    part 6

--- TowerDefense/Assets/Scripts/Health.cs
+++ TowerDefense/Assets/Scripts/Health.cs
@@ -7,13 +7,16 @@ public class Health : MonoBehaviour
     [Header("Attributes")]
     [SerializeField] private int hitPoints = 2;
 
+    private bool isDestroyed = false;
+
     public void TakeDamage(int damage)
     {
         hitPoints -= damage;
 
-        if(hitPoints <= 0)
+        if(hitPoints <= 0 && !isDestroyed)
         {
             EnemySpawner.onEnemyDestroy.Invoke();
+            isDestroyed = true;
             Destroy(gameObject);
         }
     }
